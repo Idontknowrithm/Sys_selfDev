2a196d322def701dc5ac7078c9a908e18e8d2b66
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2022-02-06 19:37:08 +0900

    Update ftl_blockmap.c

--- ftl_blockmap.c
+++ ftl_blockmap.c
@@ -1,18 +1,14 @@
-// 카피백 함수만 구현하면
-
 #include "header.h"
 
 struct blockmap_str {
 	int block_map[BLOCKS_PER_NAND];
-	int filled_pages[BLOCKS_PER_NAND][PAGES_PER_BLOCK];
-	int num_invalid_pages[BLOCKS_PER_NAND];
 
 	int current_block;	 /* current write block */
 	int current_page_offset; /* current write page offset in block */
 	int free_block_count; /* total free block count */
 	int gc_free_block; /* free block index for GC */
+	
 };
-
 struct blockmap_stats{
 	int copyback_count;
 };
@@ -29,42 +25,54 @@ static int blockmap_get_pbn(int lbn){
 	return blockmap_str.block_map[lbn];
 }
 static void blockmap_set_pbn(int lbn, int pbn){
-	int old_pbn;
-
-	assert(lbn < NUM_LBNS);
-	assert(pbn < BLOCKS_PER_NAND);
-
-	old_pbn = blockmap_str.block_map[lbn];
-	if (old_pbn < 0)
-		blockmap_str.block_map[lbn] = pbn;	
+	blockmap_str.block_map[lbn] = pbn;
 }
+static void copy_back(int lbn, int victim_block, int data){
+	int i, read_data, copyback_block;
 
+	copyback_block = blockmap_str.gc_free_block;
+	// i for copyback_page_offset
+	for(i = 0; i < PAGES_PER_BLOCK; ++i){
+		read_data = nand_page_read(victim_block, i);
+		if(read_data == data)
+			--blockmap_stats.copyback_count;
+		
+		nand_page_program(copyback_block, i, read_data);
+		++blockmap_stats.copyback_count;
+	}
+	nand_block_erase(victim_block);
+	blockmap_str.gc_free_block = victim_block;
+	blockmap_str.current_block = copyback_block;
+	blockmap_set_pbn(lbn, copyback_block);
+}
 static void blockmap_write_op(int lpn, int data){
-	int block_idx, page_offset;
+	
+	int block_idx, page_offset, read_data;
 	int pbn, lbn;
-
+	
 	page_offset = lpn % PAGES_PER_BLOCK;
-	lbn = lpn / NUM_LBNS;
+	lbn = lpn / PAGES_PER_BLOCK;
 	pbn = blockmap_get_pbn(lbn);
+
 	if(pbn < 0){
 		block_idx = blockmap_str.current_block++;
+
 		nand_page_program(block_idx, page_offset, data);
 		blockmap_set_pbn(lbn, block_idx);
-		blockmap_str.filled_pages[pbn][page_offset] = 1;
 	}
 	else{
-		if(blockmap_str.filled_pages[pbn][page_offset]){
-			copy_back();
+		if(nand_page_read(pbn, page_offset) != -1){
+			copy_back(lbn, pbn, data);
 		}
 		else{
 			nand_page_program(pbn, page_offset, data);
 		}
 	}
+	pbn = blockmap_get_pbn(lbn);
 
 	if (VERBOSE_MODE)
-		printf("%s:%d lpn %d ppn %d\n", __func__, __LINE__, lpn, pbn);
+		printf("%s:%d lpn %d pbn %d\n", __func__, __LINE__, lpn, pbn);
 }
-
 static int blockmap_read_op(int lpn){
 	int pbn, page_offset, data;
 	pbn = blockmap_get_pbn(lpn / PAGES_PER_BLOCK);
@@ -73,7 +81,6 @@ static int blockmap_read_op(int lpn){
 	data = nand_page_read(pbn, page_offset);
 	return data;
 }
-
 void blockmap_init(struct ftl_operation *op){
 	int block_idx, page_idx;
 
@@ -83,12 +90,10 @@ void blockmap_init(struct ftl_operation *op){
 	blockmap_str.gc_free_block = BLOCKS_PER_NAND - 1;
 	blockmap_str.free_block_count = BLOCKS_PER_NAND - 2;
 
-	for (block_idx = 0; block_idx < BLOCKS_PER_NAND; block_idx++){
-		blockmap_str.num_invalid_pages[block_idx] = 0;
-		blockmap_str.block_map[page_idx] = -1;
-		for(page_idx = 0; page_idx < PAGES_PER_BLOCK; ++page_idx)
-			blockmap_str.filled_pages[block_idx][page_idx] = 0;
-	}	
+	for(block_idx = 0; block_idx < BLOCKS_PER_NAND; ++block_idx)
+		nand_block_erase(block_idx);
+
+	memset(blockmap_str.block_map, -1, sizeof(blockmap_str.block_map));
 
 	op->write_op = blockmap_write_op;
 	op->read_op = blockmap_read_op;
